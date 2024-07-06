d1c501ad80d67232721c0bce89dc2b5af1b06cc7
mkbodanu4 <mailmanbo@gmail.com>
2024-07-06 11:50:37 +0300

    Cleanup and status data

--- mqtt.py
+++ mqtt.py
@@ -12,6 +12,8 @@ with open("configuration.yaml", 'r') as stream:
 
 sleep_time = configuration['run']['sleep_time']
 
+# Default values
+
 auth_token = ''
 rx_power_value = 0.0
 tx_power_value = 0.0
@@ -19,6 +21,7 @@ loid_state_value = ''
 supply_voltage_value = 0.0
 bias_current_value = 0.0
 temp_value = 0.0
+
 PonSymPerAlarm_value = ''
 PonFrameAlarm_value = ''
 PonFraPerAlarm_value = ''
@@ -26,6 +29,26 @@ PonSecSumAlarm_value = ''
 PonDygaspAlarm_value = ''
 PonLinkAlarm_value = ''
 PonCirEveAlarm_value = ''
+
+# Regular expressions
+
+rx_power_re = re.compile(r'var\ RxPower\ =\ \"(.*?)\"\;')
+tx_power_re = re.compile(r'var\ TxPower\ =\ \"(.*?)\";')
+loid_state_re = re.compile(r'Transfer\_meaning\(\'LoidState\'\,\'(.*?)\'\)\;')
+supply_voltage_re = re.compile(r'\<td\ id\=\"Frm\_Volt\"\ name\=\"Frm\_Volt\"\ class\=\"tdright\"\>(.*?)\<\/td\>')
+bias_current_re = re.compile(r'\<td\ id\=\"Frm\_Current\"\ name\=\"Frm\_Current\"[\s]{0,}class\=\"tdright\"\>(.*?)\<\/td\>')
+temp_re = re.compile(r'\<td\ id\=\"Frm\_Temp\"\ name\=\"Frm\_Temp\"\ class\=\"tdright\"\>(.*?)\<\/td\>')
+
+PonSymPerAlarm_re = re.compile(r'\<td\ id\=\"Frm_System\"\ name\=\"Frm_System\"\ class\=\"tdright\"\>(.*?)\<\/td\>')
+PonFrameAlarm_re = re.compile(r'\<td\ id\=\"Frm_Frame\"\ name\=\"Frm_Frame\"\ class\=\"tdright\"\>(.*?)\<\/td\>')
+PonFraPerAlarm_re = re.compile(r'\<td\ id\=\"Frm_FraPer\"\ name\=\"Frm_FraPer\"\ class\=\"tdright\"\>(.*?)\<\/td\>')
+PonSecSumAlarm_re = re.compile(r'\<td\ id\=\"Frm_SecSu\"\ name\=\"Frm_SecSu\"\ class\=\"tdright\"\>(.*?)\<\/td\>')
+PonDygaspAlarm_re = re.compile(r'\<td\ id\=\"Frm_Link\"\ name\=\"Frm_Link\"\ class\=\"tdright\"\>(.*?)\<\/td\>')
+PonLinkAlarm_re = re.compile(r'\<td\ id\=\"Frm_Dygasp\"\ name\=\"Frm_Dygasp\"\ class\=\"tdright\"\>(.*?)\<\/td\>')
+PonCirEveAlarm_re = re.compile(r'\<td\ id\=\"Frm_Link\"\ name\=\"Frm_Link\"\ class\=\"tdright\"\>(.*?)\<\/td\>')
+
+login_token_re = re.compile(r'getObj\(\"Frm_Logintoken\"\).value\ \=\ \"(.*?)\"\;')
+login_valid = re.compile(r'name\=\"mainFrame\"\ id\=\"mainFrame\"')
 
 hostname = configuration['mqtt']['hostname']
 auth = None
@@ -37,7 +60,7 @@ if configuration['mqtt']['username'] and configuration['mqtt']['password']:
 
 
 def topic(name_value, component='sensor'):
-    return 'homeassistant/' + component + '/ep30_' + name_value
+    return 'homeassistant/' + component + '/g55_' + name_value
 
 
 def name(sensor_name, prefix='XPON ONU ', suffix=''):
@@ -67,11 +90,13 @@ def get_gpon_status_page():
 
     response = session.request('GET', url=gpon_status_url)
     if response.status_code != 200:
+        publish_single(topic('parser_status/state'), 'Status Page Error ' + str(response.status_code))
         print("GET request to page " + gpon_status_url + " has status code " + str(response.status_code))
         return False
 
     is_logged = re.compile(r'logout\_redirect\(\)\;')
     if is_logged.search(response.text):
+        publish_single(topic('parser_status/state'), 'Not authorized')
         print("Not authorized")
         return False
 
@@ -83,6 +108,7 @@ def get_gpon_alerts_page():
 
     response = session.request('GET', url=gpon_alerts_url)
     if response.status_code != 200:
+        publish_single(topic('parser_status/state'), 'Alerts Page Error ' + str(response.status_code))
         print("GET request to page " + gpon_alerts_url + " has status code " + str(response.status_code))
         return False
 
@@ -91,23 +117,20 @@ def get_gpon_alerts_page():
 
 def parse_gpon_status_page(content):
     global rx_power_value
-    global tx_power_value
-    global loid_state_value
-    global supply_voltage_value
-    global bias_current_value
-    global temp_value
-
-    rx_power_re = re.compile(r'var\ RxPower\ =\ \"(.*?)\"\;')
     rx_power = rx_power_re.findall(content)
     if len(rx_power) > 0:
         rx_power_value = round(float(rx_power[0]) / 10000, 2)
+    else:
+        rx_power_value = 0.0
 
-    tx_power_re = re.compile(r'var\ TxPower\ =\ \"(.*?)\";')
+    global tx_power_value
     tx_power = tx_power_re.findall(content)
     if len(tx_power) > 0:
         tx_power_value = round(float(tx_power[0]) / 10000, 2)
+    else:
+        tx_power_value = 0.0
 
-    loid_state_re = re.compile(r'Transfer\_meaning\(\'LoidState\'\,\'(.*?)\'\)\;')
+    global loid_state_value
     loid_state = loid_state_re.findall(content)
     if len(loid_state) > 0:
         loid_state_value_int = loid_state[0]
@@ -124,65 +147,81 @@ def parse_gpon_status_page(content):
             loid_state_value = 'LOID conflict'
         elif loid_state_value_int == '5':
             loid_state_value = 'Registration completed'
+    else:
+        loid_state_value = ''
 
-    supply_voltage_re = re.compile(r'\<td\ id\=\"Frm\_Volt\"\ name\=\"Frm\_Volt\"\ class\=\"tdright\"\>(.*?)\<\/td\>')
+    global supply_voltage_value
     supply_voltage = supply_voltage_re.findall(content)
     if len(supply_voltage) > 0:
         supply_voltage_value = round(float(supply_voltage[0]) / 1000000, 4)
+    else:
+        supply_voltage_value = 0.0
 
-    bias_current_re = re.compile(r'\<td\ id\=\"Frm\_Current\"\ name\=\"Frm\_Current\"[\s]{0,}class\=\"tdright\"\>(.*?)\<\/td\>')
+    global bias_current_value
     bias_current = bias_current_re.findall(content)
     if len(bias_current) > 0:
         bias_current_value = round(float(bias_current[0]) / 1000, 4)
+    else:
+        bias_current_value = 0.0
 
-    temp_re = re.compile(r'\<td\ id\=\"Frm\_Temp\"\ name\=\"Frm\_Temp\"\ class\=\"tdright\"\>(.*?)\<\/td\>')
+    global temp_value
     temp = temp_re.findall(content)
     if len(temp) > 0:
         temp_value = int(temp[0])
+    else:
+        temp_value = 0.0
+
 
 def parse_gpon_alerts_page(content):
     global PonSymPerAlarm_value
-    global PonFrameAlarm_value
-    global PonFraPerAlarm_value
-    global PonSecSumAlarm_value
-    global PonDygaspAlarm_value
-    global PonLinkAlarm_value
-    global PonCirEveAlarm_value
-
-    PonSymPerAlarm_re = re.compile(r'\<td\ id\=\"Frm_System\"\ name\=\"Frm_System\"\ class\=\"tdright\"\>(.*?)\<\/td\>')
     PonSymPerAlarm = PonSymPerAlarm_re.findall(content)
     if len(PonSymPerAlarm) > 0:
         PonSymPerAlarm_value = PonSymPerAlarm[0]
+    else:
+        PonSymPerAlarm_value = ''
 
-    PonFrameAlarm_re = re.compile(r'\<td\ id\=\"Frm_Frame\"\ name\=\"Frm_Frame\"\ class\=\"tdright\"\>(.*?)\<\/td\>')
+    global PonFrameAlarm_value
     PonFrameAlarm = PonFrameAlarm_re.findall(content)
     if len(PonFrameAlarm) > 0:
         PonFrameAlarm_value = PonFrameAlarm[0]
+    else:
+        PonFrameAlarm_value = ''
 
-    PonFraPerAlarm_re = re.compile(r'\<td\ id\=\"Frm_FraPer\"\ name\=\"Frm_FraPer\"\ class\=\"tdright\"\>(.*?)\<\/td\>')
+    global PonFraPerAlarm_value
     PonFraPerAlarm = PonFraPerAlarm_re.findall(content)
     if len(PonFraPerAlarm) > 0:
         PonFraPerAlarm_value = PonFraPerAlarm[0]
+    else:
+        PonFraPerAlarm_value = ''
 
-    PonSecSumAlarm_re = re.compile(r'\<td\ id\=\"Frm_SecSu\"\ name\=\"Frm_SecSu\"\ class\=\"tdright\"\>(.*?)\<\/td\>')
+    global PonSecSumAlarm_value
     PonSecSumAlarm = PonSecSumAlarm_re.findall(content)
     if len(PonSecSumAlarm) > 0:
         PonSecSumAlarm_value = PonSecSumAlarm[0]
+    else:
+        PonSecSumAlarm_value = ''
 
-    PonDygaspAlarm_re = re.compile(r'\<td\ id\=\"Frm_Link\"\ name\=\"Frm_Link\"\ class\=\"tdright\"\>(.*?)\<\/td\>')
+    global PonDygaspAlarm_value
     PonDygaspAlarm = PonDygaspAlarm_re.findall(content)
     if len(PonDygaspAlarm) > 0:
         PonDygaspAlarm_value = PonDygaspAlarm[0]
+    else:
+        PonDygaspAlarm_value = ''
 
-    PonLinkAlarm_re = re.compile(r'\<td\ id\=\"Frm_Dygasp\"\ name\=\"Frm_Dygasp\"\ class\=\"tdright\"\>(.*?)\<\/td\>')
+    global PonLinkAlarm_value
     PonLinkAlarm = PonLinkAlarm_re.findall(content)
     if len(PonLinkAlarm) > 0:
         PonLinkAlarm_value = PonLinkAlarm[0]
+    else:
+        PonLinkAlarm_value = ''
 
-    PonCirEveAlarm_re = re.compile(r'\<td\ id\=\"Frm_Link\"\ name\=\"Frm_Link\"\ class\=\"tdright\"\>(.*?)\<\/td\>')
+    global PonCirEveAlarm_value
     PonCirEveAlarm = PonCirEveAlarm_re.findall(content)
     if len(PonCirEveAlarm) > 0:
         PonCirEveAlarm_value = PonCirEveAlarm[0]
+    else:
+        PonCirEveAlarm_value = ''
+
 
 def authenticate():
     global auth_token
@@ -190,17 +229,19 @@ def authenticate():
 
     login_form_response = session.request('GET', url=login_form_url)
     if login_form_response.status_code != 200:
+        publish_single(topic('parser_status/state'), 'Authorization Error ' + str(login_form_response.status_code))
         print("GET request to page " + login_form_url + " has status code " + str(login_form_response.status_code))
         return False
 
-    login_token_re = re.compile(r'getObj\(\"Frm_Logintoken\"\).value\ \=\ \"(.*?)\"\;')
     if not login_token_re.search(login_form_response.text):
+        publish_single(topic('parser_status/state'), 'Login token not found')
         print("Login token not found")
         return False
 
     tokens = login_token_re.findall(login_form_response.text)
 
     if len(tokens) == 0:
+        publish_single(topic('parser_status/state'), 'Login token not found')
         print("Login token not found")
         return False
 
@@ -215,11 +256,12 @@ def authenticate():
     })
 
     if login_request_response.status_code != 200:
+        publish_single(topic('parser_status/state'), 'Authorization Error ' + str(login_request_response.status_code))
         print("POST request to page " + login_form_url + " has status code " + str(login_request_response.status_code))
         return False
 
-    login_valid = re.compile(r'name\=\"mainFrame\"\ id\=\"mainFrame\"')
     if not login_valid.search(login_request_response.text):
+        publish_single(topic('parser_status/state'), 'Login unsuccessful')
         print("Login unsuccessful")
         return False
 
@@ -228,6 +270,13 @@ def authenticate():
 
 while True:
     sensors_definitions = [
+        {
+            'topic': topic('parser_status/config'),
+            'payload': json.dumps({
+                "name": name("Parser Status"),
+                "state_topic": topic('parser_status/state'),
+            })
+        },
         {
             'topic': topic('auth_token/config'),
             'payload': json.dumps({
@@ -340,6 +389,8 @@ while True:
 
     publish_multiple(sensors_definitions)
 
+    publish_single(topic('parser_status/state'), 'Working')
+
     time.sleep(sleep_time)
 
     sensors_data = []
@@ -347,6 +398,7 @@ while True:
     status_page_content = get_gpon_status_page()
 
     if not status_page_content:
+        publish_single(topic('parser_status/state'), 'Authorizing')
         print("Authorizing")
         if authenticate():
             sensors_data.append({
@@ -356,6 +408,7 @@ while True:
 
             status_page_content = get_gpon_status_page()
         else:
+            publish_single(topic('parser_status/state'), "Can't authenticate")
             print("Can't authenticate")
             exit()
 
